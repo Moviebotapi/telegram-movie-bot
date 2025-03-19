af8a0c9a34c1dfdedc671d5c3490b39d4fd4e298
Moviebotapi <Moviebot317@gmail.com>
2025-03-19 16:53:57 +0530

    Update start.js

--- src/start.js
+++ src/start.js
@@ -2,20 +2,11 @@ require('dotenv').config()
 
 const logger = require('./logger')
 
-const { BOT_TOKEN, OMDB_API_KEY, NETLIFY } = process.env
+const { BOT_TOKEN } = process.env
 
 if (!BOT_TOKEN) {
   logger.error('Fatal error: BOT_TOKEN environment variable is required!')
   process.exit()
-}
-
-if (!OMDB_API_KEY) {
-  logger.error('Fatal error: OMDB_API_KEY environment variable is required!')
-  process.exit()
-}
-
-if (NETLIFY) {
-  require('./registerWebhook')
 } else {
   require('./createBot')()
 }
